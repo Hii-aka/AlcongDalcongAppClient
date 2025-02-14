8f62a20723b3c7086a46776142684bc968ac34c6
heosangbeom <luv_3_u@naver.com>
2025-02-14 10:36:51 +0900

    add diarylist

---
+++ components/diary/DiaryList.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, View, FlatList} from 'react-native';
+import DiaryItem from './DiaryItem';
+
+const dummyData = [
+  {
+    id: 1,
+    title: '발렌타인데이 카페 데이트',
+    content: '오늘은 발렌타인데이 카페에 가서 커피를 마셨어요. 카페에서 커피를 마시면서 좋은 시간을 보냈어요.',
+    image: 'https://picsum.photos/200/300',
+    date: '2024.02.14',
+  },
+  {
+    id: 2,
+    title: '남산타워 야경 구경',
+    content: '오늘은 남산타워에서 야경을 보았어요. 반짝이는 도시의 불빛이 정말 예뻤어요. 사랑의 자물쇠도 걸고 왔답니다.',
+    image: 'https://picsum.photos/200/200',
+    date: '2024.02.13',
+  },
+]
+
+
+function DiaryList() {
+  return (
+    <FlatList
+      data={dummyData}
+      renderItem={({item}) => <DiaryItem diary={item} />}
+    />
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default DiaryList;
+
